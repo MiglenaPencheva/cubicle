d582ae5cdc294eed09be738398b2a10338c1ccf6
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-02-08 20:44:21 +0200

    add not found page

--- routs.js
+++ routs.js
@@ -10,6 +10,10 @@ const router = Router();
 router.use('/', productController);
 router.use('/about', aboutController);
 
+router.get('*', (req, res) => {
+    res.render('404', { layout: false });
+});
+
 
 
 module.exports = router;
