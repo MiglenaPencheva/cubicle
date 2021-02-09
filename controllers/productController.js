88cc087a96da7ee790f3a8b5a7e3c0ef961c1350
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-02-09 14:36:02 +0200

    parse form data

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,6 +12,11 @@ router.get('/create', (req, res) => {
     res.render('create', { title: 'Create' });
 });
 
+router.post('/create', (req, res) => {
+    console.log(req.body);
+    res.send('created');
+});
+
 router.get('/details/:productId', (req, res) => {
     res.render('details', { title: 'Product details' });
 });
