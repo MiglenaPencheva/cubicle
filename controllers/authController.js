e2578e6a1b2738d6222f6b17311bbafa265dea30
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-02-25 19:01:46 +0200

    validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,7 @@ const isAuthenticated = require('../middlewares/isAuthenticated');
 const authService = require('../services/authService');
 const { COOKIE_NAME } = require('../config/config');
 const validator = require('validator');
+const { check, validationResult } = require('express-validator');
 
 router.get('/register', isGuest, (req, res) => {
     res.render('register');
@@ -17,10 +18,10 @@ router.post('/login', isGuest, async (req, res) => {
 
     try {
         let token = await authService.login({ username, password });
-        
+
         res.cookie(COOKIE_NAME, token);
         res.redirect('/products');
-        
+
     } catch (error) {
         res.render('login', { error });
     }
@@ -30,32 +31,39 @@ router.get('/login', isGuest, (req, res) => {
     res.render('login');
 });
 
-router.post('/register', isGuest , async (req, res) => {
-    const { username, password, repeatPassword } = req.body;
-    
-    let isStrongPassword = validator.isStrongPassword(password, { 
-        minLength: 8, 
-        minLowercase: 1, 
-        minUppercase: 1, 
-        minNumbers: 1, 
-        minSymbols: 1, 
+router.post(
+    '/register',
+    isGuest,
+    // check('username', 'Username is required').notEmpty(),
+    // check('password', 'Password too short').isLength({ min: 5 }),
+    async (req, res) => {
+        const { username, password, repeatPassword } = req.body;
+
+        let isStrongPassword = validator.isStrongPassword(password, { 
+            minLength: 8, 
+            minLowercase: 1, 
+            minUppercase: 1, 
+            minNumbers: 1, 
+            minSymbols: 1, 
+        });
+
+
+        try {
+            // if (!isStrongPassword) {
+            //     throw { message: 'Stronger password required', username: req.body.username };
+            // }
+            // if (password !== repeatPassword) {
+            //     throw { message: 'Password missmatch!' };
+            // }
+
+            let user = await authService.register({ username, password });
+            res.redirect('/auth/login');
+
+        } catch (error) {
+            // let error = Object.keys(err?.errors).map(x => ({message: err.errors[x].properties.message}))[0];
+            res.render('register', { error });
+        }
     });
-
-    try {
-        if (!isStrongPassword) {
-            throw { message: 'Stronger password required' };
-        }
-        if (password !== repeatPassword) {
-            throw { message: 'Password missmatch!' }; 
-        }
-        
-        let user = await authService.register({ username, password });
-        res.redirect('/auth/login');
-
-    } catch (error) {
-        res.render('register', {error});
-    }
-});
 
 router.get('/logout', isAuthenticated, (req, res) => {
     res.clearCookie(COOKIE_NAME);
--- models/User.js
+++ models/User.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
 
+const ENGLISH_ALPHANUMERIC_PATTERN = /^[a-zA-Z0-9]+$/;
+
 const userSchema = new mongoose.Schema({
     id: mongoose.Types.ObjectId,
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => `${props.value} is invalid username`
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => `Invalid password`
+        }
     }
 });
 
